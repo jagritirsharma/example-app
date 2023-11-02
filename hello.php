125b4a936a615fef14697db9675f7f260cc6a86a
Jagriti <sharmajr@rknec.edu>
2023-11-02 20:45:14 +0530

    Update hello world program for readability and maintainability

--- hello.php
+++ hello.php
@@ -1,3 +1,8 @@
 <?php
-    echo "Hello, World!";
+
+// This line initializes a variable named $greetingMessage and assigns it the value "Hello, World!"
+$greetingMessage = "Hello, World!";
+
+// This line uses the echo statement to output the value of the $greetingMessage variable, which is "Hello, World!", to the browser or console.
+echo $greetingMessage;
 ?>
