536603e100eee786bbc4e79d477d290abf55292c
Jagriti <sharmajr@rknec.edu>
2023-11-02 20:49:47 +0530

    Update number swapping program for readability and maintainability

--- swap.php
+++ swap.php
@@ -1,9 +1,17 @@
 <?php
-$a = 5;
-$b = 10;
-echo "Before swapping: a = $a, b = $b \n";
-$temp = $a;
-$a = $b;
-$b = $temp;
-echo "After swapping: a = $a, b = $b \n";
+
+// Initialize two variables with self-descriptive names to represent the numbers to be swapped
+$firstNumber = 5;
+$secondNumber = 10;
+
+// Display the original values of the numbers
+echo "Before swapping: First number is " . $firstNumber . " and second number is " . $secondNumber . "<br>";
+
+// Swap the numbers using a temporary variable
+$temp = $firstNumber;
+$firstNumber = $secondNumber;
+$secondNumber = $temp;
+
+// Display the swapped values of the numbers
+echo "After swapping: First number is " . $firstNumber . " and second number is " . $secondNumber;
 ?>
