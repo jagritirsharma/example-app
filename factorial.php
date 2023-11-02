b1fda915bf5579c0e191c0f218e27a4bb484569b
Jagriti <sharmajr@rknec.edu>
2023-11-02 20:55:58 +0530

    Update factorial of a number program for readability and maintainability

--- factorial.php
+++ factorial.php
@@ -1,12 +1,16 @@
 <?php
-function factorial($n)
-{
-    if ($n === 0) {
-        return 1;
-    } else {
-        return $n * factorial($n - 1);
-    }
-}
+
+// Define the number for which we want to calculate the factorial
 $number = 5;
-echo "Factorial of $number is " . factorial($number);
+
+// Initialize a variable to store the factorial value
+$factorial = 1;
+
+// Iterate from 1 to the number and multiply each number to the factorial
+for ($i = 1; $i <= $number; $i++) {
+    $factorial *= $i;
+}
+
+// Display the factorial of the number
+echo "The factorial of " . $number . " is " . $factorial;
 ?>
