b555647a4bbcc8f33e1d9c30c796f7dcd54db306
Jagriti <sharmajr@rknec.edu>
2023-11-02 20:52:54 +0530

    Update number swapping programfibonacci series program for readabilityy and maintainability

--- fibonacci.php
+++ fibonacci.php
@@ -1,13 +1,23 @@
 <?php
-$n = 10;
-$first = 0;
-$second = 1;
-echo "Fibonacci Series: \n";
-echo $first . " " . $second . " ";
-for ($i = 2; $i < $n; $i++) {
-    $next = $first + $second;
-    echo $next . " ";
-    $first = $second;
-    $second = $next;
+
+// Initialize the first two numbers of the Fibonacci sequence
+$firstNumber = 0;
+$secondNumber = 1;
+
+// Display the first two numbers of the Fibonacci sequence
+echo $firstNumber . " " . $secondNumber . " ";
+
+// Set the limit for the length of the sequence
+$limit = 10;
+
+// Generate and display the Fibonacci sequence up to the specified limit
+for ($i = 2; $i < $limit; $i++) {
+    // Calculate the next number in the sequence
+    $nextNumber = $firstNumber + $secondNumber;
+    // Display the next number
+    echo $nextNumber . " ";
+    // Update the values of the variables to move to the next numbers in the sequence
+    $firstNumber = $secondNumber;
+    $secondNumber = $nextNumber;
 }
 ?>
