58eb69f6c36e33ef90db3119f487852c56307336
Jagriti <sharmajr@rknec.edu>
2023-11-22 09:46:19 +0530

    Update fibonacci program for readability and maintainability

--- practicals/fibo.php
+++ practicals/fibo.php
@@ -1,29 +1,30 @@
 <?php
-// Function to generate Fibonacci sequence up to a given number of terms
+// Function to generate Fibonacci series up to a given number of terms
 function generateFibonacci($numTerms) {
-    $fibonacciSequence = array();
+    // Initialize an array to store the Fibonacci series
+    $fibonacciSeries = array();
 
-    if ($numTerms >= 1) {
-        $fibonacciSequence[] = 0;
-    }
-    if ($numTerms >= 2) {
-        $fibonacciSequence[] = 1;
-    }
+    // First two numbers in the Fibonacci series
+    $fibonacciSeries[] = 0;
+    $fibonacciSeries[] = 1;
 
+    // Generate the Fibonacci series
     for ($i = 2; $i < $numTerms; $i++) {
-        $fibonacciSequence[] = $fibonacciSequence[$i - 1] + $fibonacciSequence[$i - 2];
+        // Calculate the next Fibonacci number by adding the last two numbers
+        $fibonacciSeries[] = $fibonacciSeries[$i - 1] + $fibonacciSeries[$i - 2];
     }
 
-    return $fibonacciSequence;
+    // Return the generated Fibonacci series
+    return $fibonacciSeries;
 }
 
-// Number of terms in the Fibonacci sequence
+// Number of terms in the Fibonacci series
 $numTerms = 10;
 
-// Generate the Fibonacci sequence
-$fibonacciSequence = generateFibonacci($numTerms);
+// Generate the Fibonacci series
+$fibonacciSeries = generateFibonacci($numTerms);
 
-// Display the generated sequence
-echo "Fibonacci sequence of $numTerms terms: " . implode(", ", $fibonacciSequence) . "\n";
+// Display the generated series
+echo "Fibonacci series of $numTerms terms: " . implode(", ", $fibonacciSeries) . "\n";
 ?>
 
